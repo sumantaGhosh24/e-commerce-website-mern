f7883b0a38b6b2d11c296e9c93eefc611c26cda3
sumanta ghosh <84187999+sumantaGhosh24@users.noreply.github.com>
2026-03-15 09:48:26 +0530

    fix build

--- client/src/app/api/apiSlice.js
+++ client/src/app/api/apiSlice.js
@@ -3,7 +3,7 @@ import {createApi, fetchBaseQuery} from "@reduxjs/toolkit/query/react";
 import {setCredentials} from "../features/auth/authSlice";
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: "http://localhost:8080/api",
+  baseUrl: "https://e-commerce-website-mern-fysf.onrender.com/api",
   credentials: "include",
   prepareHeaders: (headers, {getState}) => {
     const token = getState().auth.token;
--- server/lib/allowedOrigins.js
+++ server/lib/allowedOrigins.js
@@ -1,3 +1,7 @@
-const allowedOrigins = ["http://localhost:5173", "http://127.0.0.1:5173"];
+const allowedOrigins = [
+  "http://localhost:5173",
+  "http://127.0.0.1:5173",
+  "https://e-commerce-website-mern-1-ryrr.onrender.com",
+];
 
 module.exports = allowedOrigins;
